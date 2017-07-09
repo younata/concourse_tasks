cfc8859deb7fb072a36fc73396de781793ed585a
Rachel Brindle <you@subluminal.net>
2017-07-09 09:33:51 -0700

    Specify UTF-8 on jekyll stuff

--- tasks/jekyll_deploy_s3.sh
+++ tasks/jekyll_deploy_s3.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -el
 
+export LANG=C.UTF-8
+export LANGUAGE=C:en
+export LC_ALL=C.UTF-8
+
 cd code
 bundle install
 
--- tasks/jekyll_tests.sh
+++ tasks/jekyll_tests.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -el
 
+export LANG=C.UTF-8
+export LANGUAGE=C:en
+export LC_ALL=C.UTF-8
+
 cd code
 bundle install
 
 bundle exec jekyll build -D
-bundle exec htmlproofer ./_site
+bundle exec htmlproofer --assume-extension --internal-domains bike.younata.com ./_site
