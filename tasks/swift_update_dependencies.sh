4be08a7a4f69da71b39adf6904844330daf9c37b
Rachel Brindle <you@subluminal.net>
2018-12-09 16:25:14 -0800

    Add tasks for swift repostories to auto-update

--- tasks/swift_update_dependencies.sh
+++ tasks/swift_update_dependencies.sh
@@ -1,22 +1,19 @@
 #!/bin/bash -el
 
-export LANG=en_US.UTF-8
-export LANGUAGE=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-
 cd code
 
-bundle update
 carthage update
+swift package update
 
 if git diff-index --quiet HEAD --; then
     return
 fi
 
 bundle exec fastlane test
+swift test
 
 git add Cartfile.resolved
-git add Gemfile.lock
+git add Package.resolved
 
 git config --global user.name "$USER_NAME"
 git config --global user.email "$USER_EMAIL"
