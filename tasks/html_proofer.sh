80db2d86c207ddf57776a1fee6c0e16da1fcbcfd
Rachel Brindle <you@subluminal.net>
2019-03-24 18:51:54 -0700

    Use alpine ruby for html_proofer

--- tasks/html_proofer.sh
+++ tasks/html_proofer.sh
@@ -6,6 +6,9 @@ export LC_ALL=C.UTF-8
 
 cd code
 
+apk add --no-cache libcurl
+apk add --no-cache --virtual build-dependencies build-base libxml2-dev libxslt-dev
+
 gem install html-proofer
 
 htmlproofer --assume-extension --internal-domains $DOMAIN .
