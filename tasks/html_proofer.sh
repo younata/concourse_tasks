ae9d41002a13c6fa26cfe7d31ad11a631de5ee5f
Rachel Brindle <you@subluminal.net>
2019-03-25 08:44:15 -0700

    Make html_proofer do a bit more work

--- tasks/html_proofer.sh
+++ tasks/html_proofer.sh
@@ -6,4 +6,11 @@ export LC_ALL=C.UTF-8
 
 cd code
 
-/usr/local/bundle/bin/htmlproofer --assume-extension --internal-domains $DOMAIN .
+/usr/local/bundle/bin/htmlproofer \
+    --assume-extension \
+    --check-external-hash \
+    --check-favicon \
+    --check-html \
+    --check-img-http \
+    --enforce-https \
+    --internal-domains $DOMAIN .
