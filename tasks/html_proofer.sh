cc166cb88b4e37bf5a264d2b9cdd7e71633a6a1e
Rachel Brindle <you@subluminal.net>
2019-03-24 19:03:28 -0700

    html_proofer uses sh, not bash. Because alpine.

--- tasks/html_proofer.sh
+++ tasks/html_proofer.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -el
+#!/bin/sh -el
 
 export LANG=C.UTF-8
 export LANGUAGE=C:en
@@ -6,9 +6,4 @@ export LC_ALL=C.UTF-8
 
 cd code
 
-apk add --no-cache libcurl
-apk add --no-cache --virtual build-dependencies build-base libxml2-dev libxslt-dev
-
-gem install html-proofer
-
-htmlproofer --assume-extension --internal-domains $DOMAIN .
+/usr/local/bundle/bin/htmlproofer --assume-extension --internal-domains $DOMAIN .
