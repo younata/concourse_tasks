8a50bf7ef1b4777d887b0cb95bb1c42baec9b6de
Rachel Brindle <you@subluminal.net>
2020-10-23 09:53:31 -0700

    don't enclose command check in square bracket

--- tasks/python_tests.sh
+++ tasks/python_tests.sh
@@ -4,13 +4,13 @@ cd code
 
 pip install -r requirements.txt
 
-if [ command -v invoke >/dev/null 2>&1 ]; then
+if command -v invoke >/dev/null 2>&1; then
     echo 'Running `invoke test`'
     invoke test
-elif [ command -v pytest >/dev/null 2>&1 ]; then
+elif command -v pytest >/dev/null 2>&1; then
     echo 'Running `pytest`'
     pytest
 else
-    echo 'No supported test runner supported (looking for invoke or pytest)'
+    echo 'No supported test runner found (looking for invoke or pytest)'
     exit 1
 fi
