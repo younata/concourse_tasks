8f207d6f98ae65756ffca65e19b1c02c39831754
Rachel Brindle <you@subluminal.net>
2020-12-22 16:47:47 -0800

    html_proofer prints what directory it's checking

--- tasks/html_proofer.sh
+++ tasks/html_proofer.sh
@@ -4,13 +4,15 @@ export LANG=C.UTF-8
 export LANGUAGE=C:en
 export LC_ALL=C.UTF-8
 
-directory="$SUBDIRECTORY"
+directory="."
 
-if [ -n "${SUBDIRECTORY+set}" ]; then
-    directory='.'
+if [ "$SUBDIRECTORY" = "" ]; then
+    directory="$SUBDIRECTORY"
 fi
 
 cd code
+
+echo "Checking all files in $directory"
 
 /usr/local/bundle/bin/htmlproofer \
     --assume-extension \
