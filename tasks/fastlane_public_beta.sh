aa1437355754b964caa557072f2e591cd8416f2b
Rachel Brindle <you@subluminal.net>
2021-05-08 10:31:35 -0700

    Public betas via fastlane

---
+++ tasks/fastlane_public_beta.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -el
+
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export FASTLANE_SKIP_UPDATE_CHECK=1
+
+cd code
+bundle
+bundle exec fastlane public_beta
